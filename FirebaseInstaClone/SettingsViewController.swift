30aaeab8ccbdb3cb67abd4a9c05d61a74422344c
mertozan <mertozan1997@gmail.com>
2024-11-27 03:11:44 +0300

    Proje başlangıcı: Firebase ile e-posta giriş işlemleri tamamlandı

---
+++ FirebaseInstaClone/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Mert ÖZAN on 27.11.2024.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
+    }
+
+
+}
